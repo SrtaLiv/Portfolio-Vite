8f290aebba88cccbfc5d2d541e4d780f3f568cac
Olivia Todesco <104026140+SrtaLiv@users.noreply.github.com>
2024-09-08 16:30:00 -0300

    emprolije el nav, falta arreglar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,8 @@
-import React, { useContext, useState } from 'react';
 import { DarkModeContext } from "../../context/darkModeContext";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Link } from 'react-scroll';
 import "./navbar.scss";
-import { color } from 'framer-motion';
+import { useContext, useState } from "react";
 
 function Nav() {
     const { toggle, darkMode } = useContext(DarkModeContext);
@@ -13,50 +12,48 @@ function Nav() {
         console.log('click');
         setMenuOpen(!menuOpen);  // Alterna el estado del menú
     };
-
     return (
         <header>
-            <nav className='nav'>
+            <nav>
                 <Link className='nav-link' to="inicio" smooth={true} duration={600}>
                     <h3 className='logo'>Olivia</h3>
                 </Link>
 
-                <div className={`nav-menu ${menuOpen ? 'open' : ''}`}>
-                    <ul className='nav-list'>
-                        <li className='burguer' onClick={handleMenuToggle}>
-                            <FontAwesomeIcon icon="fa-solid fa-bars" />
-                        </li>
-
-                        <li className='nav-item'>
+                <ul>
+                    <div className={`nav-menu ${menuOpen ? 'open' : ''}`}>
+                        <li>
                             <Link className='nav-link' to="sobre-mi" smooth={true} duration={600} onClick={handleMenuToggle}>
                                 Sobre mi
                             </Link>
                         </li>
 
-                        <li className='nav-item'>
+                        <li>
                             <Link className='nav-link' to='proyectos' smooth={true} duration={600} onClick={handleMenuToggle}>
                                 Proyectos
                             </Link>
                         </li>
 
-                        <li className='nav-item'>
+                        <li>
                             <Link className='nav-link' to='contacto-container' smooth={true} duration={600} onClick={handleMenuToggle}>
                                 Contacto
                             </Link>
                         </li>
 
-                        <li className='nav-item'>
-                            {darkMode ? (
-                                <FontAwesomeIcon onClick={toggle} icon="fa-solid fa-sun" />
-                            ) : (
-                                <FontAwesomeIcon onClick={toggle} icon="fa-solid fa-moon" />
-                            )}
-                        </li>
-                    </ul>
-                </div>
+                    </div>
+
+                    <li>
+                        {darkMode ? (
+                            <FontAwesomeIcon onClick={toggle} icon="fa-solid fa-sun" />
+                        ) : (
+                            <FontAwesomeIcon onClick={toggle} icon="fa-solid fa-moon" />
+                        )}
+                    </li>
+                    <li className='burguer' onClick={handleMenuToggle}>
+                        <FontAwesomeIcon icon="fa-solid fa-bars" />
+                    </li>
+                </ul>
             </nav>
-        </header>
+        </header >
     );
 }
-
 export default Nav;
