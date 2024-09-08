27ecb9a8804d65258127788500f12b57b01b42f5
Olivia Todesco <104026140+SrtaLiv@users.noreply.github.com>
2024-09-08 16:42:38 -0300

    agrego span color y en visitados

--- src/components/Sobre Mi/Sobremi.jsx
+++ src/components/Sobre Mi/Sobremi.jsx
@@ -8,9 +8,9 @@ function Sobremi() {
             <div className='descripcion'>
                 <img alt="Foto perfil" draggable="true" src="https://pbs.twimg.com/profile_images/1802921319207407616/9X6R2I2f_400x400.jpg" />
                 <p>
-                    Mi nombre es <span>Ana Olivia Todesco</span>, soy de Argentina y me especializo en el Desarrollo Web.
+                    Mi nombre es <span>Ana Olivia Todesco</span>, soy de Argentina y me especializo en el Desarrollo Web. Me apasiona la tecnología y la programación, y disfruto de aprender cosas nuevas cada día.
                     <br /><br />
-                    Actualmente, estudio una Tecnicatura Universitaria en Desarrollo de Aplicaciones Informáticas para continuar mi formación. En cada desafío doy lo mejor de mí y busco crecer como profesional en este ámbito. Además de estudiar, creo contenido en <a href="https://www.instagram.com/oliviatodesco/" target="_blank" rel="noopener noreferrer">Instagram</a>, donde recomiendo recursos para programadores y diseñadores.
+                    Actualmente, estudio una <span>Tecnicatura Universitaria en Desarrollo de Aplicaciones </span>Informáticas para continuar mi formación. En cada desafío doy lo mejor de mí y busco crecer como profesional en este ámbito. Además de estudiar, creo contenido en <a href="https://www.instagram.com/oliviatodesco/" target="_blank" rel="noopener noreferrer">Instagram</a>, donde recomiendo recursos para programadores y diseñadores.
                     <br /><br />
                 </p>
             </div>
