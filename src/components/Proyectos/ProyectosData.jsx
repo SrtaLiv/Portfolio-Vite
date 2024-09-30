75eb00c3a2b90de6f0bc7c1ec1c4c626f7e96f2c
Olivia Todesco <104026140+SrtaLiv@users.noreply.github.com>
2024-09-30 18:59:18 -0300

    cambio estilos de proyectos

--- src/components/Proyectos/ProyectosData.jsx
+++ src/components/Proyectos/ProyectosData.jsx
@@ -2,16 +2,16 @@ const proyectosData = [
     {
         id: 1,
         titulo: 'SaveFood',
-        descripcion: 'Aplicacion de registro de comidas. Aun en desarrollo.',
+        descripcion: 'SaveFood es una aplicación web diseñada para personas que desean registrar sus comidas diarias sin enfocarse en el conteo de macronutrientes. El objetivo principal de la aplicación es promover la consciencia alimentaria de manera saludable, permitiendo a los usuarios reflejar sus hábitos de alimentación sin la presión de contar calorías o nutrientes, lo que la convierte en una herramienta ideal para quienes viven con Trastornos de la Conducta Alimentaria (TCA).',
         tecnologias: ['Java', 'Spring Boot', 'Angular', 'Primeng', 'SCSS', 'Docker', 'MySQL'],
         imagenUrl: 'https://sacfa.org.za/wp-content/uploads/333754160_1189495331931687_4124014204074738876_n.jpg',
         enlace: 'https://github.com/SrtaLiv/SaveFood-Frontend.git'
     },
     {
         id: 2,
-        titulo: 'Portfolio',
-        descripcion: 'Mi portfolio personal, hecho con mucho amor y dedicación.',
-        tecnologias: ['React', 'SCSS', 'JS'],
+        titulo: 'Landing Page para Nutricionista',
+        descripcion: 'Página web diseñada para una nutricionista. El objetivo principal de la página es brindar información sobre los servicios que ofrece la profesional, así como también facilitar el contacto con los pacientes.',
+        tecnologias: ['React', 'SCSS'],
         imagenUrl: 'https://sacfa.org.za/wp-content/uploads/333754160_1189495331931687_4124014204074738876_n.jpg',
         enlace: 'https://github.com/SrtaLiv/Portfolio-v1.git'
     },
